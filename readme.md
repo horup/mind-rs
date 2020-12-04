d1ea92212e5e3311de4332fea556a40fdc88a40d
horup <sah@sah.dk>
2020-12-04 22:49:57 +0100

    initial

--- readme.md
+++ readme.md
@@ -1,15 +1,11 @@
-Goal of mind is to improve concentration and idea generation using a simple game.
-The application will ask the player several questions in a row, which must be answared fast and thus requires concentration, and randomly ask the player to do some thinking task.
-The aim is to get the player into a 'flow mode' through several simple maths tests and when it is detected the player is in flow he is asked to think about an idea or a solution for a problem.
-The aim is to allow the player to go into flow mode and to provide the player the ability to generate new ideas and see connections while in said flow mode.
-The aim is to get the player into flow mode and thus generate new connections between ideas.
-The aim is to get the player into flow mode and thus generate new innovative ideas and see connection between existing ideas.
-The aim is to generate new innovatie ideas through flow inducing gameplay.
+An experimental crude mind training game written in Rust, which might or might not improve flow state of the mind.
 
-The project is a success when:
-- the game tries to induce 'flow' into the player.
+The goal of the game is to repeat what is shown on the screen as fast as possible.
+Every now an then, the game will ask you to think about some topic and then continue.
 
-- the game ask the player random questions such as:
- - what ideas are in your mind right now?
- - what connections do you see which you did not see before?
- - what problems are worth solving?
+questions.txt must be in the same folder as the executable.
+questions.txt can be extended with additional questions that make sense for you.
+
+The game was inspired by the book: Stealing Fire, which talks about altered states of the mind...
+
+It is currently unknown if the game does indeed help with flow state, or if it is 'a waste of time'.
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::io::{self, BufRead};
 use device_query::{DeviceState, Keycode};
 use rand::thread_rng;
 use rand::seq::SliceRandom;
+use std::io::stdout;
+use crossterm::{ExecutableCommand, cursor::MoveTo, Result, style::Print, terminal::Clear};
 
 
 fn math_exercise(device_state:&mut DeviceState) {
     let a = rand::random::<u32>() % 3 + 1;
-    println!("{}", a);
+    print_top(&format!("{}\n", a)).unwrap();
 
     let mut success = false;
     loop {
@@ -21,14 +23,16 @@ fn math_exercise(device_state:&mut DeviceState) {
 
         if success && keys.iter().count() == 0 {
             break;
+        } else {
+            sleep(Duration::from_millis(1));
         }
     }
 }
 
 fn think_of(think_of:&mut usize, questions:&Vec<String>) {
 
-    println!("{}", &questions.get(*think_of % questions.len()).unwrap());
-    println!("------");
+    let q = &questions.get(*think_of % questions.len()).unwrap();
+    print_top(&format!("{}\n", q)).unwrap();
     loop {
         let t = *think_of+1;
         if t != *think_of {
@@ -40,7 +44,7 @@ fn think_of(think_of:&mut usize, questions:&Vec<String>) {
     sleep(Duration::from_secs(10));
 }
 
-fn main() {
+fn load_questions() -> Vec<String> {
     let mut questions:Vec<String> = Vec::new();
     let file = File::open("./questions.txt").unwrap();
     let reader = io::BufReader::new(file).lines();
@@ -49,7 +53,21 @@ fn main() {
     }
 
     questions.shuffle(&mut thread_rng());
+    questions
+}
 
+fn print_top(s:&str) -> Result<()> {
+    let mut out = stdout();
+    out.execute(Clear(crossterm::terminal::ClearType::All))?;
+    sleep(Duration::from_millis(100));
+    out.execute(MoveTo(0,0))?;
+    out.execute(Print(s))?;
+    out.execute(MoveTo(0,1))?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let questions = load_questions();
     let mut now = Instant::now();
     let mut device_state = DeviceState::new();
     let mut iteration = 1;
@@ -61,10 +79,13 @@ fn main() {
             if t >= questions.len() {
                 let duration = Instant::now() - now;
                 now = Instant::now();
-                println!("All done! Game took {:?}", duration);
+                stdout().execute(Print("Hello World"))?;
+                print_top(&format!("All done! Game took {:?}. Starting a new game in 10 seconds...", duration))?;
+                sleep(Duration::from_secs(10));
                 t = 0;
             }
         }
+        sleep(Duration::from_millis(100));
         math_exercise(&mut device_state);
         iteration+=1;
     }
